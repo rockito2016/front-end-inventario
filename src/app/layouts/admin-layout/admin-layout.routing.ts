3692ef469e6e501bdbd1b21d2d18c0a7065bba29
Javier Alejandro Pascuaza Cordoba <javieral.pascuaza@umariana.edu.co>
2024-09-22 18:14:01 -0500

    InitialRoutes

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -4,7 +4,6 @@ import { StockComponent } from '../../pages/stock/stock.component';
 import { PedidoComponent } from '../../pages/pedido/pedido.component';
 import { CompraComponent } from '../../pages/compra/compra.component';
 import { BalanceCompraComponent } from '../../pages/compra/balance-compra/balance-compra.component';
-import { Component } from '@angular/core';
 import { HistorialCompraComponent } from '../../pages/compra/historial-compra/historial-compra.component';
 import { VentaComponent } from '../../pages/venta/venta.component';
 import { BalanceVentaComponent } from '../../pages/venta/balance-venta/balance-venta.component';
@@ -17,41 +16,49 @@ import { ProductFormComponent } from '../../pages/producto/product-form/product-
 import { ProductListComponent } from '../../pages/producto/product-list/product-list.component';
 
 export const AdminLayoutRouter: Routes = [
-  {path: 'dashboard',               component: DashboardComponent},
-  {path: 'stock',                   component: StockComponent},
-  {path: 'pedido',                  component: PedidoComponent, children: [
-    {
-      path: 'proveedor',            component: ProveedorComponent
-    },
-    {
-      path: 'transportador',        component: TransportadorComponent
-    }
-  ]},
-  {path: 'compra',                  component: CompraComponent, children: [
-    {
-      path: 'balance-compra',       component: BalanceCompraComponent
-    },
-    {
-      path: 'historial-compra',     component: HistorialCompraComponent
-    }
-  ]},
-  {path: 'venta',                   component: VentaComponent, children: [
-    {
-      path: 'balance-venta',        component: BalanceVentaComponent
-    },
-    {
-      path: 'historial-compra',     component: HistorialVentaComponent
-    }
-  ]},
-  {path: 'producto',                component: ProductoComponent, children: [
-    {
-      path: 'producto-detail',      component: ProductDetailComponent
-    },
-    {
-      path: 'product-form',         component: ProductFormComponent
-    },
-    {
-      path: 'product-list',         component: ProductListComponent
-    }
-  ]}
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'stock', component: StockComponent },
+  {
+    path: 'pedido', component: PedidoComponent, children: [
+      {
+        path: 'proveedor', component: ProveedorComponent
+      },
+      {
+        path: 'transportador', component: TransportadorComponent
+      }
+    ]
+  },
+  {
+    path: 'compra', component: CompraComponent, children: [
+      {
+        path: 'balance-compra', component: BalanceCompraComponent
+      },
+      {
+        path: 'historial-compra', component: HistorialCompraComponent
+      }
+    ]
+  },
+  {
+    path: 'venta', component: VentaComponent, children: [
+      {
+        path: 'balance-venta', component: BalanceVentaComponent
+      },
+      {
+        path: 'historial-compra', component: HistorialVentaComponent
+      }
+    ]
+  },
+  {
+    path: 'producto', component: ProductoComponent, children: [
+      {
+        path: 'producto-detail', component: ProductDetailComponent
+      },
+      {
+        path: 'product-form', component: ProductFormComponent
+      },
+      {
+        path: 'product-list', component: ProductListComponent
+      }
+    ]
+  }
 ]
